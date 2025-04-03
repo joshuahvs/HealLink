928c9eddfcbaaa37c1482abd5b05af16b5ede4a6
vito1012buss <thebestvitodiaries@gmail.com>
2025-04-03 22:21:06 +0700

    change font and config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,28 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        darkblue: '#3C70FF',
+        blue: '#3CA1FF',
+        lightblue: '3CA1FF',
+        green: '9FE076',
       },
+
+      fontSize: {
+        'h1': '64px',
+        'h2': '32px',
+        'h3': '24px', 
+        'h4': '20px',
+        'h1-mob': ['48px', {fontWeight: '700'}],
+        'h2-mob': ['24px', {fontWeight: '700'}],
+        'h3-mob': ['20px', {fontWeight: '700'}],
+        'h4-mob': ['18px', {fontWeight: '700'}],
+        'lg': ['18px', {fontWeight: '400'}],
+        'md': ['14px', {fontWeight: '400'}],
+        'sm': ['12px', {fontWeight: '400'}],
+        'lg-mob': '14px',
+        'md-mob': '12px',
+        'sm-mob': '10px',
+      }
     },
   },
   plugins: [],
